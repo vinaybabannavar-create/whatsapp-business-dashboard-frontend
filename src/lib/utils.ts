6b8fda49068eca86955a285438fbf48c39f8267b
vinaybabannavar <vinaybabannavar@gmail.com>
2025-12-10 11:17:47 +0530

    Fix merge conflicts in utils and globals.css

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
+// src/lib/utils.ts
+
 export function cn(...classes: (string | false | null | undefined)[]) {
   return classes.filter(Boolean).join(" ");
 }
-=======
-export function cn(...classes: (string | false | null | undefined)[]) {
-  return classes.filter(Boolean).join(" ");
-}
->>>>>>> 977c3b3b9697ba4d9c58d2047bd14e2e5a6ef096
